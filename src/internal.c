51abb583fe1944bacef7f83501d3bb4355a54642
Amo39 <145296702+Amo39@users.noreply.github.com>
2024-10-03 14:52:42 -0400

    Create internal.c

---
+++ src/internal.c
@@ -0,0 +1,21 @@
+#include "internal.h"
+
+void cd_command(tokenlist *tokens){
+        if(tokens->size > 2){   //checks for 2 or more arguments
+                printf("To many arguments\n");
+        }
+        else if(tokens->size ==1){   //when it finds only on argument
+                char* home = getenv("HOME"); //get home env
+                if(home!= NULL){  //checks for NULL
+                        if(chdir(home) != 0)
+                                perror("failure with cd");
+                }
+        }
+        else{      // checks to see if the file/directory exist
+                if(chdir(tokens->items[1]) != 0){
+                        perror("file does no exist");
+                }
+
+        }
+        return;
+}
