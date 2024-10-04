4b71f0a91ae8f26c9b2c671855fd9834ed547439
Amo39 <145296702+Amo39@users.noreply.github.com>
2024-10-03 23:50:28 -0400

    Create Ev.c

---
+++ src/Ev.c
@@ -0,0 +1,18 @@
+#include "Ev.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+void expand_variable(char **token) {
+    if((*token)[0] == '$') {
+        char* envName = *token + 1;
+        char* envValue = getenv(envName);
+
+        if(envValue != NULL) {
+            free(*token);
+            *token = malloc(strlen(envValue)+ 1);
+
+            strcpy(*token, envValue);
+        }
+    }
+}
